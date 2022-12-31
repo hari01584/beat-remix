ab55f63aab38e3e9e9934dd2dc4e1be45327923a
Harishankar Kumar <hari01584@gmail.com>
2022-12-31 20:59:11 +0530

    change some code files

--- beat.py
+++ beat.py
@@ -1,5 +1,6 @@
 from image_selector import datasetImages, _datasetImages
 from sound_selector import datasetSound, _datasetSound
+from scripts.chooser import get_choice, choose
 
 # --- Get Sound --- #
 # sound = _datasetSound("sounds", "restmycase.mp3")
@@ -15,8 +16,18 @@ timestamps = butter.knife() # Return results
 # images = _datasetImages(len(timestamps), "images", "animepos", 0)
 images = datasetImages(len(timestamps))
 
+# --- Ask info ---- #
+video_specs = {
+    "1080x1920 (Landscape) - (Instagram, Reels, Portrait)":"1080:1920",
+    "1920x1080 (Wide, 16:9) - (Youtube)": "1920:1080",
+    "1080x720 (Normal, 4:3)": "1080:720",
+}
+lst = [*video_specs]
+choice = choose(lst)
+dimen = video_specs[lst[choice]]
+print("You chose:", lst[choice], "thf. dimen passed:", dimen)
 # --- Merge timestamp --- #
 from honey.simple_honey_merger import SimpleHoneyMerger
-honey = SimpleHoneyMerger(timestamps, images, sound)
+honey = SimpleHoneyMerger(timestamps, images, sound, dimen)
 honey.honey()
 honey.toast()
--- honey/simple_honey_merger.py
+++ honey/simple_honey_merger.py
@@ -12,11 +12,12 @@ def _addSection(inf, filename, tracktime):
     inf.append("outpoint %s"%(tracktime))
 
 class SimpleHoneyMerger:
-    def __init__(self, timestamp, images, sound):
+    def __init__(self, timestamp, images, sound, dimen):
         self.timestamp = timestamp
         self.images = cycle(images)
         self._toast = []
         self.sound = sound
+        self.dimen = dimen
 
     def honey(self):
         print("Starting simple honey merger")
@@ -31,7 +32,9 @@ class SimpleHoneyMerger:
         if not os.path.exists("output/"):
             os.makedirs("output/")
 
-        command = "ffmpeg -y -i %s -f concat -safe 0 -i %s -c:v libx264 -pix_fmt yuv420p -vf scale=320:240 -acodec copy -shortest %s" % (self.sound, "in.txt", "output/"+os.path.splitext(os.path.basename(self.sound))[0]+".mp4")
+        # command = "ffmpeg -y -i %s -f concat -safe 0 -i %s -c:v libx264 -pix_fmt yuv420p -vf scale=320:240 -acodec copy -shortest %s" % (self.sound, "in.txt", "output/"+os.path.splitext(os.path.basename(self.sound))[0]+".mp4")
+        command = "ffmpeg -y -noautorotate -i %s -f concat -safe 0 -i %s -c:v libx264 -pix_fmt yuv420p -vf scale=%s:force_original_aspect_ratio=decrease:eval=frame,pad=%s:-1:-1:color=black -acodec copy -shortest %s" % (self.sound, "in.txt", self.dimen, self.dimen, "output/"+os.path.splitext(os.path.basename(self.sound))[0]+".mp4")
+        
         print(command)
 
         print("Starting builder final")
